1356a6900d2a0d8b67f3c5ffb32e30ebee97ecc1
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2025-05-02 12:09:16 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,22 +5,38 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   const select_button = document.getElementById("horn-select");
+  const horn_image = document.querySelector("#expose img");
+  const audio = document.querySelector("audio");
+  const volume = document.querySelector("#volume-controls img")
   const volume_button = document.getElementById("volume");
   const button = document.querySelector("button");
   let sound = null;
   let image = "no-image.png";
-  
+
 
   select_button.addEventListener('change', (event) => {
-
-    console.log("Selected horn:", event.target.value);
+    const selected = event.target.value;
+    horn_image.src = `assets/images/${selected}.svg`;
+    horn_image.alt = `${selected} image`;
+    audio.src = `assets/audio/${selected}.mp3`;
   });
 
   volume_button.addEventListener('input', (event) => {
-    console.log("Volume level:", event.target.value);
+    const val = event.target.value;
+    audioElement.volume = val / 100;
+
+    let icon = "volume-level-0.svg";
+    if (val >= 67) icon = "volume-level-3.svg";
+    else if (val >= 33) icon = "volume-level-2.svg";
+    else if (val >= 1) icon = "volume-level-1.svg";
+
+    volume.src = `assets/icons/${icon}`;
+    volume.alt = `Volume level ${icon.charAt(icon.length - 5)}`;
   });
 
   button.addEventListener('click', () => {
-    console.log("Play Sound button clicked");
+    if (audio.src) {
+      audio.play();
+    }
   });
 }
