fe74fde99036b9980b29c959952318844386d3de
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2025-05-02 11:47:16 -0700

    print out values

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const select_button = document.getElementById("horn-select");
+  const volume_button = document.getElementById("volume");
+  const button = document.querySelector("button");
+  let sound = null;
+  let image = "no-image.png";
+  
+
+  select_button.addEventListener('change', (event) => {
+
+    console.log("Selected horn:", event.target.value);
+  });
+
+  volume_button.addEventListener('input', (event) => {
+    console.log("Volume level:", event.target.value);
+  });
+
+  button.addEventListener('click', () => {
+    console.log("Play Sound button clicked");
+  });
 }
