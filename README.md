64951e13597bf9cb27b1b5f2a835beb5c9c72068
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2025-05-02 12:54:33 -0700

    first draft of explore

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Lab 5 - Starter
 Name: Joshua Caneday
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+Answer: 
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Answer: 
+
+Expose: https://joshcaneday.github.io/Lab5_Starter/expose.html
+Explore: https://joshcaneday.github.io/Lab5_Starter/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,47 @@
-// explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  const textArea = document.getElementById('text-to-speak');
+  const faceImg = document.querySelector('#explore img');
+
+  const synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoices();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  speakButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textArea.value);
+    const selectedIndex = voiceSelect.value;
+
+    if (selectedIndex !== 'select') {
+      utterance.voice = voices[selectedIndex];
+    }
+
+    utterance.onstart = () => {
+      faceImg.src = 'assets/images/open.png';
+    };
+
+    utterance.onend = () => {
+      faceImg.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterance);
+  });
 }
